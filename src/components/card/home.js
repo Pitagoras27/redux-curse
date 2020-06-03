cde763b1657a78c8ec7042716c2986c2c05a6486
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-06-03 13:46:29 -0500

    Se instala redux-thunk para dar soporte al action creator que devuelve una función en lugar de un objeto plano

--- src/components/card/home.js
+++ src/components/card/home.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import * as getAll from '../../actions';
+// import * as getAll from '../../actions';
+import { getAll } from '../../actions';
 import Users from './rows';
 import './home.css';
 
 class Home extends Component {
-  // const [count, setCount] = useState(0);
   constructor(props) {
     super(props)
     this.state = {
@@ -15,11 +15,12 @@ class Home extends Component {
   }
 
   async componentDidMount() {
-    const fetchData = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await fetchData.json();
-    this.setState({
-      users: data,
-    })
+    // const fetchData = await fetch('https://jsonplaceholder.typicode.com/users');
+    // const data = await fetchData.json();
+    // this.setState({
+    //   users: data,
+    // })
+    this.props.getAll()
   }
   
   render() {
@@ -44,4 +45,7 @@ class Home extends Component {
 }
 
 const mapStateToProps = state => state.usariosReducer;
-export default connect(mapStateToProps, getAll)(Home);
+const mapDispatchToProps = {
+  getAll,
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
 import './index.css';
 import App from './components';
 
-const store = createStore(reducers, {});
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(reduxThunk)
+);
 
 ReactDOM.render(
   <Provider store={store}>
