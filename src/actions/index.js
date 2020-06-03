acd26630ae9bfb31ad08d57947823d88afb01aad
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-06-03 14:53:27 -0500

    Se ejemplifica como no funciona correctamente la gestión de excepciones con fetch nativo

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
 import { ACTIONS_NAMES } from '../constants';
 
 export const getAll = () => async dispatch => {
   try {
-    const fetchData = await axios('https://jsonplaceholder.typicode.com/users');
+    const fetchData = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await fetchData.json();
     dispatch({
       type: ACTIONS_NAMES.getUsers,
-      payload: fetchData.data,
+      payload: data,
     }) 
   } catch (error) {
     console.log('error:', error.message);
