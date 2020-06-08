c04f62580491f8f88a838a3ef5629295846f14e4
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-06-08 18:27:03 -0500

    Se muestran las listas de tarea en la UI

--- src/components/Todos/index.js
+++ src/components/Todos/index.js
@@ -1,19 +1,60 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Spinner from '../spinner/spinner';
+import Fail from '../Fail';
 import { getTodosAction } from '../../actions/getTodos';
+import '../../css/components/todoLIst.css';
 class Todos extends Component {
   
   componentDidMount() {
     this.props.getTodosAction();
   }
 
-  render() {
-    console.log('on todos component! ->', this.props)
-    return (
-      <div>Todos!</div>
-    )
-  }
+  showContent = () => {
+		const { todos, loadingTodos, errorTodos } = this.props.todos;
+
+		if (loadingTodos) {
+			return <Spinner />
+		}
+		if (errorTodos) {
+			return <Fail mensaje={ errorTodos } />
+		}
+
+		return Object.keys(todos).map((userId) => (
+			<div key={ userId }>
+				<h2>Usuario { userId }</h2>
+				<div>
+					{ this.setTodoUI(userId) }
+				</div>
+			</div>
+		));
+	};
+
+	setTodoUI = (userId) => {
+		const { todos } = this.props.todos;
+		const forUser = {
+			...todos[userId]
+		};
+
+		return Object.keys(forUser).map((todoId) => (
+			<div key={ todoId }>
+				<input type='checkbox'
+					defaultChecked={ forUser[todoId].completed }
+				/>
+				{ forUser[todoId].title }
+			</div>
+		));
+	};
+
+	render() {
+		console.log(this.props.tareas);
+		return (
+			<div className='main-container-todolist'>
+				{ this.showContent() }
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => ({
